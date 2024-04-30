cd60d8a7b8d8d3193509881723b9936264354121
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-01 00:44:48 +0530

    Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import React, { Fragment, useState } from "react";
 
-import { useState } from "react";
 function App() {
+  // Correction: Changed 'cons' to 'const' in useState declaration
+  const [title, setTitle] = useState("My name is somesh");
 
-  const [count, setCount] = useState(0);
-  function onclickHandler() {
-    setCount(count + 1);
+  function updateTitle() {
+    // Correction: Removed space before Math.random() for correct string concatenation
+    setTitle("My name is " + Math.random());
   }
 
-
   return (
-    <div>
-      <button onClick={onclickHandler}>Counter {count}</button>
-    </div>
-  )
+    <Fragment>
+      {/* Correction: Removed extra curly braces around updateTitle */}
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title}></Header>
+      <Header title="Somesh 2"></Header>
+    </Fragment>
+  );
 }
 
-export default App
+function Header({ title }) {
+  return <div>{title}</div>;
+}
+
+export default App;
