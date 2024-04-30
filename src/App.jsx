a6b535c1c1b75e5a6abd4242e7c7e4ed2e1bd6d3
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-01 01:09:23 +0530

    Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
 import React, { Fragment, useState } from "react";
 
 function App() {
-  // Correction: Changed 'cons' to 'const' in useState declaration
-  const [title, setTitle] = useState("My name is somesh");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Go to gym",
+      description: "Go to gym today"
+    },
+    {
+      id: 2,
+      title: "Go to gym",
+      description: "Go to gym today"
+    },
+    {
+      id: 3,
+      title: "Eat Food",
+      description: "Eat food"
+    },
+    {
+      id: 4,
+      title: "Go to class",
+      description: "Go to class today"
+    }
+  ]);
 
-  function updateTitle() {
-    // Correction: Removed space before Math.random() for correct string concatenation
-    setTitle("My name is " + Math.random());
+  function addTodo() {
+    const newTodos = [...todos]; // Correction: Initialize newTodos as a copy of todos array
+    newTodos.push({
+      id: todos.length + 1, // Correction: Ensure unique id by using todos length
+      title: "Task " + Math.random(), // Correction: Changed Math.random() to string
+      description: "Description " + Math.random() // Correction: Changed Math.random() to string
+    });
+    setTodos(newTodos);
   }
 
   return (
-    <Fragment>
-      {/* Correction: Removed extra curly braces around updateTitle */}
-      <button onClick={updateTitle}>Update the title</button>
-      <Header title={title}></Header>
-      <Header title="Somesh 2"></Header>
-    </Fragment>
+    <div>
+      <button onClick={addTodo}>Add to todo</button>
+      {todos.map(todo => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
+    </div>
   );
 }
 
-function Header({ title }) {
-  return <div>{title}</div>;
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 }
 
 export default App;
